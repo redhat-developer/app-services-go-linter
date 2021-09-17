ab79380533935bd167a5cc4ad8cf80f73cd6973a
Alex Rodin <alexander.a.rodin@gmail.com>
2021-09-17 18:30:15 -0400

    feat: Base code to run analyzer

--- cmd/go-i18n-linter/main.go
+++ cmd/go-i18n-linter/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"github.com/alexal/go-i18n-linter/pkg/analyzer"
+	"golang.org/x/tools/go/analysis/singlechecker"
+)
 
+func main() {
+	singlechecker.Main(analyzer.Analyzer)
 }
--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -1,1 +1,15 @@
 package analyzer
+
+import (
+	"golang.org/x/tools/go/analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "go-i18n-linter",
+	Doc:  "",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	return nil, nil
+}
