68b8723f5467fdf03eec30c6cc0e2921d8f56522
Alex Rodin <alexander.a.rodin@gmail.com>
2021-09-17 18:47:29 -0400

    feat: Base code to run analyzer

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -1,15 +1,43 @@
 package analyzer
 
 import (
+	"fmt"
+	"go/ast"
 	"golang.org/x/tools/go/analysis"
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name: "go-i18n-linter",
-	Doc:  "",
+	Name: "goi18nlinter",
+	Doc:  "goi18nlinter",
 	Run:  run,
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	inspect := func(node ast.Node) bool {
+		switch n := node.(type) {
+		case *ast.CallExpr:
+			s, ok := n.Fun.(*ast.SelectorExpr)
+			if !ok {
+				return true
+			}
+
+			if s.Sel.Name == "MustLocalize" || s.Sel.Name == "MustLocalizeError" {
+				fmt.Println(s.Sel.Name)
+				if len(n.Args) > 0 {
+					args, ok := n.Args[0].(*ast.BasicLit)
+					if !ok {
+						return true
+					}
+					fmt.Println(args.Value)
+				}
+			}
+		}
+		return true
+	}
+
+	for _, f := range pass.Files {
+		ast.Inspect(f, inspect)
+	}
+
 	return nil, nil
 }
