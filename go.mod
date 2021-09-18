df33e5a324d47c639651335546052596db6f213e
Alex Rodin <alexander.a.rodin@gmail.com>
2021-09-17 23:19:37 -0400

    feat: added filters

--- go.mod
+++ go.mod
@@ -1,11 +1,11 @@
 module github.com/alexal/go-i18n-linter
 
-go 1.17
+go 1.16
 
 require golang.org/x/tools v0.1.6
 
 require (
-	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/mod v0.5.0 // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -4,40 +4,47 @@ import (
 	"fmt"
 	"go/ast"
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name: "goi18nlinter",
-	Doc:  "goi18nlinter",
-	Run:  run,
+	Name:             "goi18nlinter",
+	Doc:              "goi18nlinter",
+	Run:              run,
+	Requires:         []*analysis.Analyzer{inspect.Analyzer},
+	RunDespiteErrors: true,
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
-	inspect := func(node ast.Node) bool {
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{
+		(*ast.CallExpr)(nil),
+		(*ast.SelectorExpr)(nil),
+		(*ast.BasicLit)(nil),
+	}
+
+	inspector.Preorder(nodeFilter, func(node ast.Node) {
 		switch n := node.(type) {
 		case *ast.CallExpr:
 			s, ok := n.Fun.(*ast.SelectorExpr)
 			if !ok {
-				return true
+				return
 			}
 
 			if s.Sel.Name == "MustLocalize" || s.Sel.Name == "MustLocalizeError" {
 				fmt.Println(s.Sel.Name)
+				pass.Reportf(1, s.Sel.Name)
 				if len(n.Args) > 0 {
 					args, ok := n.Args[0].(*ast.BasicLit)
 					if !ok {
-						return true
+						return
 					}
 					fmt.Println(args.Value)
 				}
 			}
 		}
-		return true
-	}
-
-	for _, f := range pass.Files {
-		ast.Inspect(f, inspect)
-	}
+	})
 
 	return nil, nil
 }
